521db7e1b0b9f0f81e51225a2758753d654d8c70
Ron David <rondavid209@gmail.com>
2023-06-21 11:38:52 -0700

    10 X alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabets from a-z x10
+ *
+ * return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
+
+
+
+
+
