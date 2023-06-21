8664b02b1cf4f42f593cbb9a1eb9cf91842f25d1
Ron David <rondavid209@gmail.com>
2023-06-21 07:53:15 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "alxse.h"
 
 /**
- * main - entry point function
+ * main - prints the string "_putchar" from a character array.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
