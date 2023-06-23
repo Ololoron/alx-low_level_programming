99d4b63441d4312715167c8689c2d27c99dd7ff1
Ololoron <rondavid209@gmail.com>
2023-06-23 15:59:54 +0300

    Update History.c

--- 0x00-hello_world/History.c
+++ 0x00-hello_world/History.c
@@ -7,7 +7,10 @@
 */
 int main(void)
 {
-	puts("In the early '70s we migrated from Uganda as a community due to the civil wars at the time. The Kabaka wasn't in good terms with the tribes leader of our then very minute community. Something had to be done, and that something saw us occupy the lonely islands of Mfangano, Rusinga and some parts of the mainland that is The Gwasi Highlands.\n");
+	puts("In the early '70s we migrated from Uganda as a community due to the civil wars at the time. 
+		The Kabaka wasn't in good terms with the tribes leader of our then very minute community. 
+		Something had to be done, and that something saw us occupy the lonely islands of Mfangano, 
+		Rusinga and some parts of the mainland that is The Gwasi Highlands.\n");
 
 return (0);
 }
