0582e6d645ab2c2b97dc92edcb983ce4598ce37d
Ron David <rondavid@rondavid-VirtualBox>
2023-07-11 16:07:17 +0300

    Float like a butterfly, string like a bee.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,8 +16,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int a;
 	char *s;
 
-	s = (char *) malloc(size * sizeof(char));
-	if (s == NULL)
+	s = malloc(size * sizeof(char));
+	if (s == NULL || s == NULL)
 		return (NULL);
 	for (a = 0; a < size; a++)
 	{
