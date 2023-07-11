5f0ef5448608d24d05358097d6d805f380bc6a40
Ron David <rondavid@rondavid-VirtualBox>
2023-07-11 16:11:41 +0300

    Float like a butterfly, string like a bee.

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -3,31 +3,31 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - print buffer in hexa
+ * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
- * Return: Nothing
+ * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+    unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
 }
 
 /**
@@ -37,17 +37,15 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-	char *buffer;
-	unsigned int size;
-	
-	size = 98;
-	buffer = create_array(size, 'H');
-	if (buffer == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return(1);
-	}
-	simple_print_buffer(buffer, size);
-	free(buffer);
-	return (0);
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
 }
