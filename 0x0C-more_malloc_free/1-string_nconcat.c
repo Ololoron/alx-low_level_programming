3a433a3e3e131eae660779746b4e809f7e641db6
Ron David <rondavid@rondavid-VirtualBox>
2023-07-13 23:22:17 +0300

    array_range

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates 2 strings
+ *
+ * @s1: First string
+ * @s2: Second string.
+ * @n: unsinged integer 
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *point;
+	unsigned int i = 0, j = 0, k;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[j])
+	{
+		j++;
+	}
+	if (n >= j)
+		n = j;
+	j = 0;
+	point = malloc(sizeof(char) * (i + n + 1));
+	if (point == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+	{
+		point[k] = s1[k];
+	}
+	for (; k < (i + n); k++)
+	{
+		point[k] = s2[k - i];
+	}
+	point[k] = '\0';
+	return (point);
+}
